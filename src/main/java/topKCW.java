ca647b4c00b78e13c4294c3b8422e7c7a9da9808
Alen K S <alen@Alens-MacBook-Pro.local>
2020-09-20 07:01:46 +0800

    top 20 words without duplicates

--- src/main/java/topKCW.java
+++ src/main/java/topKCW.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -35,8 +37,8 @@ public class topKCW {
 
 
 
-    public static class IntSumReducer
-            extends Reducer<Text,Text,IntWritable,Text> {
+    public static class IntSumReducer<K extends WritableComparable, V extends Writable>
+            extends Reducer<Text,Text,Text,IntWritable> {
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<Text> values,
@@ -51,27 +53,97 @@ public class topKCW {
             if (WordFileCtr.size() == 2) {
                 int min_sum = Collections.min(WordFileCtr.values());
                 result.set(min_sum);
-                context.write(result, key);
+                context.write(key, result);
             }
         }
     }
 
+    public static class SFMapper
+            extends Mapper<Object, Text, Text, Text>{
+        private TreeMap<Integer, String> tmap = new TreeMap<Integer, String>();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String[] tokens = value.toString().split("\t");
+            String word = tokens[0];
+            Integer count = -1 * Integer.parseInt(tokens[1]);
+            tmap.put(count, word);
+            if (tmap.size() > 20) {
+                tmap.remove(tmap.firstKey());
+            }
+        }
+        @Override
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            for (Map.Entry<Integer, String> entry : tmap.entrySet()) {
+                Integer count = entry.getKey();
+                String word = entry.getValue();
+                context.write(new Text(word), new Text(Integer.toString(count)));
+            }
+        }
+    }
+
+    public static class SFReducer
+            extends Reducer<Text,Text,Text,Text> {
+        private TreeMap<Integer, String> tmap2 = new TreeMap<Integer, String>();
+
+        public void reduce(Text key, Iterable<Text> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            String word = key.toString();
+            Integer count = 0;
+
+            for (Text val : values)
+            {
+                count = Integer.parseInt(val.toString());
+            }
+            tmap2.put(count, word);
+            if (tmap2.size() > 20)
+            {
+                tmap2.remove(tmap2.firstKey());
+            }
+        }
+        @Override
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            for (Map.Entry<Integer, String> entry : tmap2.entrySet()) {
+                Integer count = -1 * entry.getKey();
+                String name = entry.getValue();
+                context.write(new Text(Integer.toString(count)), new Text(name));
+            }
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
+        String tempDir =  "tmp";
+
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "top k word count");
-        job.setJarByClass(topKCW.class);
-        job.setMapperClass(TokenizerMapper.class);
+        Job job1 = Job.getInstance(conf, "top k word count");
+        job1.setJarByClass(topKCW.class);
+        job1.setMapperClass(TokenizerMapper.class);
         //job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileInputFormat.addInputPath(job, new Path(args[1]));
-        FileInputFormat.addInputPath(job, new Path(args[2]));
-        FileOutputFormat.setOutputPath(job, new Path(args[3]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        job1.setReducerClass(IntSumReducer.class);
+        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputValueClass(Text.class);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileInputFormat.addInputPath(job1, new Path(args[1]));
+        FileInputFormat.addInputPath(job1, new Path(args[2]));
+        FileOutputFormat.setOutputPath(job1, new Path(tempDir));
+
+        job1.waitForCompletion(true);
+
+        Configuration conf2 = new Configuration();
+        Job job2 = Job.getInstance(conf2, "sort and filter");
+        job2.setJarByClass(topKCW.class);
+        job2.setMapperClass(SFMapper.class);
+        job2.setReducerClass(SFReducer.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job2, new Path(tempDir));
+        FileOutputFormat.setOutputPath(job2, new Path(args[3]));
+
+        System.exit(job2.waitForCompletion(true) ? 0 : 1);
     }
 
 }
